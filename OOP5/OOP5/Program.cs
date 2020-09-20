2c4e2c8663b971a90da9262d90691c96b7467574
postovalovICQ <71002789+postovalovICQ@users.noreply.github.com>
2020-09-21 00:03:56 +0500

    Add files via upload

---
+++ OOP5/OOP5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace OOP5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Постовалов", "Денис");
+            Employee employee = new Employee("Лицо", "Рука", "КУПК");
+            person.Display();
+            employee.Display();
+            Console.ReadKey();
+        }
+    }
+    class Person
+    {
+        private string _firstName;
+        private string _lastName;
+        public Person(string name, string surname)
+        {
+            _firstName = name;
+            _lastName = surname;
+        }
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public void Display()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+    }
+    class Employee : Person
+    {
+        private string _company;
+
+        public Employee(string name, string surname, string company) : base(name, surname)
+        {
+            Company = company;
+        }
+        public string Company
+        {
+            get { return _company; }
+            set { _company = value; }
+        }
+    }
+
+}
